daa4de8d0a7039a8469399cb2d6ba6ad14259088
Jerrod Anderson <jerrod_anderson@taylor.edu>
2021-11-07 12:18:50 -0800

    Schema and initial population done

---
+++ upland-weather/knexfile.js
@@ -0,0 +1,47 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      host: 'pg.cse.taylor.edu',
+      user: 'jerrod_anderson',
+      password: 'yiniluxu',
+      database: 'jerrod_anderson'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
